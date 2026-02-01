3d134e3bced7f055636dc4bb6663335c3bdd4191
silvvquiroz <silvana.quiroz@pucp.edu.pe>
2026-02-01 12:48:52 -0500

    WorldBank scraper V.01

--- src/main/java/WebScraper.java
+++ src/main/java/WebScraper.java
@@ -1,7 +1,14 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.io.IOException;
 
 public class WebScraper {
@@ -9,15 +16,24 @@ public class WebScraper {
     private String baseURLOffShore = "https://offshoreleaks.icij.org/";
     private String baseURLWorlBank = "https://projects.worldbank.org/en/projects-operations/procurement/debarred-firms";
 
-    public void searchOffShore(String entity) {
+    /**
+     *
+     * @param entity
+     */
+    public void searchOffShore(final String entity) {
+        // 0. Setear los parámetros de búsqueda
+        int from = 0;
+        int resultsPerPage = 100;
+
+        String trailParams = "&c=&j=&d=&cat=Entity";
+        String searchEntity = entity.trim().replace(" ", "+");
+
         try {
-            // 0. Setear los parámetros de búsqueda
-            int from = 0;
-            int resultsPerPage = 100;
-
-            String trailParams = "&c=&j=&d=&cat=Entity";
-            String searchEntity = entity.trim().replace(" ", "+");
-
+            /*
+            Sidenote: La página de Off Shore muestra los resultados en una
+            tabla que va cargando bloques de 100 filas. Por ello,
+            se modifica la URL para obtener el HTML de cada bloque de 100 en iteraciones
+             */
             while (true) {
                 // 1. Construir la URL con parámetros de búsqueda
                 String searchURL = baseURLOffShore + "search?q=" + searchEntity + trailParams + "&from=" + from;
@@ -81,8 +97,58 @@ public class WebScraper {
 
     }
 
-    public void searchWorldBank(String entity) {
+    /**
+     *
+     * @param entity
+     */
+    public void searchWorldBank(final String entity) {
+        // 0. Configurar el web driver (en este caso, ChromeDriver) y los parámetros de búsqueda
 
+        // Establecer ChromeDriver como el driver por defecto de WebDriverManager
+        WebDriverManager.chromedriver().setup();
+
+        // Configurar ChromeDriver para trbajar con Selenium
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+
+        // Iniciar el WebDriver con ChromeDriver
+        WebDriver driver = new ChromeDriver(options);
+
+        String searchEntity = entity.trim();
+
+        try {
+            // 1. Conectar a la URL de WorldBank
+            driver.get(baseURLWorlBank);
+
+            // 2. Localizar el textbox de búsqueda
+            /*
+            Sidenote: El nombre de la entidad se ingresa en un elemento de forma:
+            tag: <input>
+            id: "category"
+             */
+            WebElement textBox = driver.findElement(By.id("category"));
+
+            // 3. Ingresar el texto a buscar (nombre de la entidad)
+            textBox.sendKeys(searchEntity);
+
+            // Esperar a que la tabla se actualice por completo
+            Thread.sleep(2000);
+
+            // 4. Obtener el HTML actualizado y seleccionar las filas de la tabla
+            WebElement table = driver.findElement(By.id("k-debarred-firms"));
+
+            // Testing: Imprimir el HTML actualizado
+            String html = table.getAttribute("outerHTML");
+            System.out.println(html);
+
+        }
+        catch (RuntimeException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            // x. Cerrar el driver de Chrome después de procesar
+            driver.quit();
+        }
     }
 
     public static void main(String[] args) {
@@ -90,7 +156,8 @@ public class WebScraper {
 
         WebScraper ws = new WebScraper();
         String entityTesting = "AERO CONTINENT";
-        ws.searchOffShore(entityTesting);
+        //ws.searchOffShore(entityTesting);
+        ws.searchWorldBank(entityTesting);
 
         System.out.println("Test Finished!");
     }
