86bab6900f31f7e9284fe0c132e64607756a2837
silvvquiroz <silvana.quiroz@pucp.edu.pe>
2026-02-04 17:14:27 -0500

    Local API change

--- src/main/java/com/scrapx/api/ScrapX.java
+++ src/main/java/com/scrapx/api/ScrapX.java
@@ -2,6 +2,9 @@ package com.scrapx.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Clase principal que arranca la aplicación Spring Boot.
@@ -11,7 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * estén disponibles para ser utilizados.
  */
 @SpringBootApplication
-public class ScrapX {
+public class ScrapX implements WebMvcConfigurer {
 
     /**
      * Método principal que inicia la aplicación Spring Boot.
@@ -21,5 +24,15 @@ public class ScrapX {
     public static void main (String[] args) {
         // Ejecutar la aplicación Spring Boot
         SpringApplication.run(ScrapX.class, args);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permitir solicitudes desde localhost y Vercel
+        registry.addMapping("/**") // Configura todas las rutas para permitir CORS
+                .allowedOrigins("http://localhost:5173", "https://supplier-hub-front.vercel.app") // Los orígenes permitidos
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos los encabezados
+                .allowCredentials(true); // Permitir credenciales si es necesario
     }
 }
